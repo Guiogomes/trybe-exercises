2c50b8cd63904a574c27c463d1ecd8bd83325424
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-05-19 19:51:36 -0300

    adiciona e cumpre o exercicio 2 de fixacao

---
+++ exercises/bloco_4/dia_1/exercicio-2.js
@@ -0,0 +1,12 @@
+// Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let num1 = 10;
+let num2 = 30;
+
+if (num1 > num2) {
+    console.log (num1)
+} else if (num2 > num1) {
+    console.log (num2)
+} else {
+    console.log ('caso não se aplica')
+}
