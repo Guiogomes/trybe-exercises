9160270a1b898c645fc140de51d03383be09f9c4
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-05-20 19:19:34 -0300

    cria e resolve o exercicio 3 dos exercicios de fixacao

---
+++ exercises/bloco_4/dia_2/exercicio-3.js
@@ -0,0 +1,10 @@
+// Para o terceiro exercício, calcule e imprima a 
+// média aritmética dos valores contidos no array;
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let somaNumbers = 0; 
+for (let soma = 0; soma < numbers.length; soma +=1) {
+    somaNumbers=numbers[soma] + somaNumbers;
+    let mediaAritimetica = somaNumbers / numbers.length
+    console.log (mediaAritimetica);
+}
