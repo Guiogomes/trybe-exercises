79286f78b9cda6363ae1591e1958b61f42efa4e1
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-05-20 19:26:11 -0300

    cria e resolve o exercicio 4 dos exercicios de fixacao

--- exercises/bloco_4/dia_2/exercicio-4.js
+++ exercises/bloco_4/dia_2/exercicio-4.js
@@ -1,10 +1,11 @@
-// Para o terceiro exercício, calcule e imprima a 
-// média aritmética dos valores contidos no array;
-
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let somaNumbers = 0; 
 for (let soma = 0; soma < numbers.length; soma +=1) {
     somaNumbers=numbers[soma] + somaNumbers;
     let mediaAritimetica = somaNumbers / numbers.length;
-    console.log (mediaAritimetica);
+    if (mediaAritimetica > 20) {
+        console.log ('valor maior que 20.');
+    } else {
+        console.log ('valor menor ou igual a 20.')
+    }
 }
