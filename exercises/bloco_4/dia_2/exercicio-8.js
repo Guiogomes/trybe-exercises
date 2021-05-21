ffcb5221744548c52607f919f4d877873d189730
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-05-21 10:33:31 -0300

    realizado o exercicio de fixacao 8

---
+++ exercises/bloco_4/dia_2/exercicio-8.js
@@ -0,0 +1,11 @@
+// Utilizando for , crie um array que vá de 1 até 25 e imprima o resultado;
+
+let numeros = [];
+for (let num = 1; num <= 25; num  +=1){
+    let valor = num;
+    if (num <= 25){
+        numeros.push (valor)
+    }
+    
+}
+console.log (numeros);
