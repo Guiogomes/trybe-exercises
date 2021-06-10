7afb160990cdb9d8397e3d66cbabe797bea038ed
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-10 16:28:51 -0300

    comeca as resolucoes da parte de javascript do dia 6.1

--- exercises/bloco_6/dia_1/script.js
+++ exercises/bloco_6/dia_1/script.js
@@ -7,9 +7,61 @@ function createStateOptions(params) {
     // dentro de cada posição criar uma opção
     const createOptionOfState = document.createElement('option');
     createOptionOfState.innerText = statePosition;
+    createOptionOfState.id = statePosition;
     document.getElementById('state-select').appendChild(createOptionOfState);
   }
 
 }
 createStateOptions();
 
+function getInputsData() {
+  const inputItems = document.getElementsByTagName('input');
+  const inputValuesSave = [];
+  for (let index = 0; index < inputItems.length; index += 1) {
+    const inputItemsObjectForm = {
+      name: inputItems[index].name,
+      id: inputItems[index].id,
+      content: inputItems[index].value,
+    };
+    inputValuesSave.push(inputItemsObjectForm);
+  }
+  return inputValuesSave;
+}
+getInputsData();
+
+function validateInfoInputs() {
+  const getDateInput = document.getElementById('beginning-input').value;
+  let dayOfGetDateInput = getDateInput[0]+getDateInput[1];
+  let monthOfGetDateInput = getDateInput[3]+getDateInput[4];
+  let yearOfGetDateInput = getDateInput[6]+getDateInput[7]+getDateInput[8]+getDateInput[9];
+  let ocurrancyOfNumbers = /0123456789/g;
+ if(dayOfGetDateInput !== ocurrancyOfNumbers) {
+  alert('entre com um formato válido de dia');
+  return;
+ } else if (parseInt(dayOfGetDateInput) < 0) {
+   alert('entre com um formato válido de dia');
+   return;
+ } else if (parseInt(dayOfGetDateInput > 31)) {
+  alert('entre com um formato válido de dia');
+  return;
+ } else if(monthOfGetDateInput !== ocurrancyOfNumbers){
+  alert('entre com um formato válido de mês');
+  return;
+ } else if (parseInt(monthOfGetDateInput) < 1) {
+  alert('entre com um formato válido de mês');
+  return;
+ } else if (parseInt(monthOfGetDateInput) > 12) {
+   alert('entre com um formato válido de mês');
+  return;
+ } else if (yearOfGetDateInput !== ocurrancyOfNumbers) {
+  alert('entre com um formato válido de ano');
+  return;
+ } else if (parseInt(yearOfGetDateInput) < 1900) {
+  alert('entre com um formato válido de ano');
+  return;
+ } else if (parseInt(yearOfGetDateInput) > 2005) {
+   alert('entre com um formato válido de ano');
+  return;
+ }
+}
+validateInfoInputs();
