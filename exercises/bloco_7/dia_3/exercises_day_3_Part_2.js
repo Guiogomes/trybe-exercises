9900fd2504f8f69eaf807014b595808fc5f0e326
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-22 12:15:53 -0300

    resolve os exercicios da parte 2 do dia 7.3

--- exercises/bloco_7/dia_3/exercises_day_3_Part_2.js
+++ exercises/bloco_7/dia_3/exercises_day_3_Part_2.js
@@ -34,3 +34,50 @@ const expected2 = [3, 6, 10, 5];
 assert.strictEqual(typeof wordLengths, 'function');
 const output2 = wordLengths(words);
 assert.deepStrictEqual(output2, expected2);
+
+const sumAllNumbers = (array) => {
+	let sumNumbers =0;
+	for (let index = 0; index < array.length; index += 1) {
+		if (array[index] !== 'number'){
+			sumNumbers += 0;
+		}
+		sumNumbers += array[index];		
+	}
+	return sumNumbers;
+}
+
+const numbers = [9, 23, 10, 3, 8];
+const expected3 = 53;
+const output3 = sumAllNumbers(numbers);
+
+assert.strictEqual(typeof sumAllNumbers, 'function');
+assert.strictEqual(output3, expected3);
+
+const findTheNeedle = (array, word) => {
+	let position = 0;
+	if (array.includes(word) !== true) {
+		position = -1;
+	}
+	for (let index = 0; index < array.length; index += 1) {
+		if (array[index] === word) {
+			position = index;
+			return
+		}		
+	}
+	return position;
+}
+
+let wordsFindTheNeedle = ['house', 'train', 'slide', 'needle', 'book'];
+let expected4 = 3;
+let output4 = findTheNeedle(wordsFindTheNeedle, 'needle');
+assert.strictEqual(output4, expected4);
+
+wordsFindTheNeedle = ['plant', 'shelf', 'arrow', 'bird'];
+expected4 = 0;
+output4 = findTheNeedle(wordsFindTheNeedle, 'plant');
+assert.strictEqual(output4, expected4);
+
+wordsFindTheNeedle = ['plant', 'shelf', 'arrow', 'bird'];
+expected4 = -1;
+output4 = findTheNeedle(wordsFindTheNeedle, 'plat');
+assert.strictEqual(output4, expected4);
