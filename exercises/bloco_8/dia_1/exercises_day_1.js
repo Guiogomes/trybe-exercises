8a534af677f6f67aa017500505242b8aee65d909
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-23 22:01:44 -0300

    resolve o exercicio de fixacao 2

--- exercises/bloco_8/dia_1/exercises_day_1.js
+++ exercises/bloco_8/dia_1/exercises_day_1.js
@@ -31,3 +31,32 @@ console.log(newEmployees(generateDate));
 // apostado é igual ao número sorteado. O retorno da sua HOF deve ser 
 // uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
 
+const resultBet = (number) => {
+	if (typeof(number)!== 'number') {
+		console.log('Não é possível analisar o resultado sem um número.');
+		return;
+	}
+	const compare = (number) => {
+		if (number === Math.floor(Math.random()*6)) {
+			console.log('Parabéns você ganhou');
+		} else {
+			console.log('Tente novamente');
+		}
+	}
+	compare(number);
+}
+
+resultBet(5);
+
+// 3 - Crie uma HOF que receberá três parâmetros. 
+// O primeiro será um array de respostas corretas (Gabarito), 
+// o segundo será um array de respostas a serem verificadas 
+// (respostas da pessoa estudante) e o terceiro é uma função que 
+// checa se as respostas estão corretas e faz a contagem da pontuação 
+// final recebida pela pessoa estudante. Ao final a HOF deve retornar 
+// o total da contagem de respostas certas. Quando a resposta for correta
+// a contagem sobe 1 ponto, quando for incorreta desce 0.5 pontos, e 
+// quando não houver resposta ("N.A") não altera-se a contagem.
+
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
