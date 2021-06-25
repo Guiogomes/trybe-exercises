fd388397a79520cf8f6ff26df616dcd7af8a9a8d
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-25 14:47:08 -0300

    comeca o exercicio 3 do dia 8.2

--- exercises/bloco_8/dia_2/exercise_3.js
+++ exercises/bloco_8/dia_2/exercise_3.js
@@ -64,3 +64,23 @@ const books = [
 ];
 
 // Adicione o código do exercício aqui:
+
+// Encontre o primeiro livro 
+// cujo nome possui 26 caracteres.
+
+const expectedResult = {
+  author: {
+    birthYear: 1948,
+    name: 'George R. R. Martin',
+  },
+  genre: 'Fantasia',
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  releaseYear: 1991,
+};
+
+function getNamedBook() {
+  // escreva seu código aqui
+}
+
+assert.deepStrictEqual(getNamedBook(), expectedResult);
