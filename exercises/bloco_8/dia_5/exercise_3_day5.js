8d0278775eccc0695a08c8d2b888b6932fb29a9c
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-29 20:54:23 -0300

    resolve a questao 3 do dia 8.5

---
+++ exercises/bloco_8/dia_5/exercise_3_day5.js
@@ -0,0 +1,23 @@
+const assert = require('assert');
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+
+// complete a assinatura da função abaixo
+const {name: personName, age: personAge, likes: personLike} = alex;
+const {name, age, likes} = gunnar;
+const personLikes = (args1, args2, args3) => `${args1} is ${args2} years old and likes ${args3.join(', ')}.`;
+
+assert.strictEqual(personLikes(personName,personAge,personLike), 'Alex is 26 years old and likes fly fishing.');
+assert.strictEqual(personLikes(name, age, likes), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.');
