fbcf7da8cbbeeda65fa461df27e821f234d2064a
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-06-29 21:02:49 -0300

    resolve a questao 4 do dia 8.5

--- exercises/bloco_8/dia_5/exercise_3_day5.js
+++ exercises/bloco_8/dia_5/exercise_3_day5.js
@@ -19,5 +19,5 @@ const {name: personName, age: personAge, likes: personLike} = alex;
 const {name, age, likes} = gunnar;
 const personLikes = (args1, args2, args3) => `${args1} is ${args2} years old and likes ${args3.join(', ')}.`;
 
-assert.strictEqual(personLikes(personName,personAge,personLike), 'Alex is 26 years old and likes fly fishing.');
+assert.strictEqual(personLikes(personName, personAge, personLike), 'Alex is 26 years old and likes fly fishing.');
 assert.strictEqual(personLikes(name, age, likes), 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.');
