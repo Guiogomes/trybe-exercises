87984ba9e8cdec65713aae58d6289a508a152787
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-14 18:21:11 -0300

    Salva os arquivos de fixação do dia 2 do bloco 10

--- exercises/bloco_10/dia_2/test/script.test.js
+++ exercises/bloco_10/dia_2/test/script.test.js
@@ -1,8 +1,21 @@
-test("Não deveria passar!", () done => {
+const SumNumbers = (a, b, callback) => {
   setTimeout(() => {
-    expect(10).toBe(5);
-    console.log('Deveria falhar!')
-    done();
-  }, 500);
-});
+    const result = a + b;
+    callback(result);
+  }, 500)
+}
 
+// test("Não deveria passar!", () done => {
+//   setTimeout(() => {
+//     expect(10).toBe(5);
+//     console.log('Deveria falhar!')
+//     done();
+//   }, 500);
+// });
+
+test('Testando SumNumbers, soma 5 mais 10', done => {
+  SumNumbers(5, 10, (result) => {
+    expect(result).toBe(15);
+    done();
+  });
+});
