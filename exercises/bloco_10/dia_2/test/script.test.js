63b4035d8a1bd89276870f5437b8500ed8f0b796
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-14 15:09:15 -0300

    Muda o local do diretorio do dia 2

--- exercises/bloco_10/dia_2/test/script.test.js
+++ exercises/bloco_10/dia_2/test/script.test.js
@@ -1,7 +1,8 @@
-test("Não deveria passar!", () => {
+test("Não deveria passar!", () done => {
   setTimeout(() => {
     expect(10).toBe(5);
     console.log('Deveria falhar!')
     done();
   }, 500);
 });
+
