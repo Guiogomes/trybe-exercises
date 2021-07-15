edd1b1405745a477602086a94b28a5efd7c8924e
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-15 15:12:48 -0300

    comeca o exercicio 2 e 3 do dia 2

---
+++ exercises/bloco_10/dia_2/test/getUserNameAsyncAwait.test.js
@@ -0,0 +1,25 @@
+const { getUserNameAsyncAwait } = require('../getUserNameAsyncAwait');
+
+
+describe('Successful cases of the function getUserName', () => {	
+	it('expects that passing 4 as string the function will behave as expected', async() => {
+		await expect(getUserNameAsyncAwait('4')).resolves.toBe('Mark');
+	});
+	it('expects that passing 5 as string the function will behave as expected', async () => {
+		await expect(getUserNameAsyncAwait('5')).resolves.toBe('Paul');
+	});
+	it('expects that passing 4 as number the function will behave as expected', async () => {
+		await expect(getUserNameAsyncAwait(4)).resolves.toBe('Mark');
+	});
+	it('expects that passing 5 as number the function will behave as expected', async () => {
+		await expect(getUserNameAsyncAwait(5)).resolves.toBe('Paul');
+	});
+});
+
+// describe('Failure cases of the function getUserName', () => {
+// 	it('Throw error with a invalid parameter', async () => {
+// 		await getUserNameAsyncAwait('4').catch(error => {
+// 			expect(error).toEqual({ error: 'User with xablau not found.' })
+// 		});
+// 	});
+// });
