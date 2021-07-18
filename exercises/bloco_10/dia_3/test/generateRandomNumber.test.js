fdb575458ea4d47c9d43624bf2681643ac0fc76c
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-18 16:35:38 -0300

    Atualiza os packages e resolve a questao 3

--- exercises/bloco_10/dia_3/test/generateRandomNumber.test.js
+++ exercises/bloco_10/dia_3/test/generateRandomNumber.test.js
@@ -3,15 +3,23 @@ let generateRandomNumbers = require('../generateRandomNumber');
 describe('Behavior of the function generateRandomNumbers', () => {
 	generateRandomNumbers = jest
 		.fn()
-		.mockReturnValue(10)
+	
 	test('Verify with the function has been called', () => {
 		generateRandomNumbers();
 		expect(generateRandomNumbers).toHaveBeenCalled();
 	});
 	test('Verify if called generateRandomNumbers with mockValue if he has 10 with value', () => {
+		generateRandomNumbers.mockReturnValue(10);
 		expect(generateRandomNumbers()).toBe(10);
 	});
 	test('Verify number of times is the generateRandomNumbers has been called', () => {
 		expect(generateRandomNumbers).toHaveBeenCalledTimes(2)
+	});
+});
+
+describe('Behavior of the function generateRandomNumber with mock cases with pass parameters from her', () => {
+	test ('Expect with mock format the function generateRandomNumbers show a division result with the parameters', () => {
+		generateRandomNumbers.mockImplementation((number1, number2) => number1 / number2);
+		expect(generateRandomNumbers(4,2)).toBe(2);
 	});
 });
