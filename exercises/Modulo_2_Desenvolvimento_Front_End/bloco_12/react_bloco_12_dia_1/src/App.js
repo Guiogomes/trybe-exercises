7138e551cf42575fa2c2ae3670969ca8744cfca8
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-07-26 14:31:31 -0300

    muda o codigo da Pokedex, preciso ajustar erros

--- exercises/Modulo_2_Desenvolvimento_Front_End/bloco_12/react_bloco_12_dia_1/src/App.js
+++ exercises/Modulo_2_Desenvolvimento_Front_End/bloco_12/react_bloco_12_dia_1/src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* Embora isso funcione, essa DEFINITIVAMENTE
+não é a maneira correta de se criar eventos
+em React! A função se refere ao componente,
+então deve ser parte de sua classe! */
+
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = { 
+      clickNumber: 0,
+      clickNumberBtn2: 0,
+      clickNumberBtn3: 0
+    }
+    
+    this.handleClick = this.handleClick.bind(this);
+    this.handleClickBtn2 = this.handleClickBtn2.bind(this);
+    this.handleClickBtn3 = this.handleClickBtn3.bind(this);
+  }
+
+  handleClick () {
+    this.setState((previousNumberOfClicks, _props) => ({
+      clickNumber: previousNumberOfClicks.clickNumber + 1,
+    }));
+  };
+
+  handleClickBtn2 () {
+    this.setState((previousNumberOfClicksBtn2, _props) => ({
+      clickNumberBtn2: previousNumberOfClicksBtn2.clickNumberBtn2 + 1,
+    }));
+  };
+
+  handleClickBtn3 () {
+    this.setState((previousNumberOfClicksBtn3, _props) => ({
+      clickNumberBtn3: previousNumberOfClicksBtn3.clickNumberBtn3 + 1,
+    }));
+  };
+
+
+  render() {
+    return(
+      <div>
+        <button onClick={this.handleClick}>botão 1: {this.state.clickNumber}</button>   
+        <button onClick={this.handleClickBtn2}>botão 2: {this.state.clickNumberBtn2}</button>   
+        <button onClick={this.handleClickBtn3}>botão 3: {this.state.clickNumberBtn3}</button>   
+      </div>
+    )}
 }
 
 export default App;
