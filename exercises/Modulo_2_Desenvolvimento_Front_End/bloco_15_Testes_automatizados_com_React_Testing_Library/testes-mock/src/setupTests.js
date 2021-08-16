1934f1c2542018a027a4bf88b7225631203b510c
Guilherme Gomes <ggs.arquiteto@gmail.com>
2021-08-16 20:44:33 -0300

    termina os exercicios de fixacao do conteudo da aula

--- exercises/Modulo_2_Desenvolvimento_Front_End/bloco_15_Testes_automatizados_com_React_Testing_Library/testes-mock/src/setupTests.js
+++ exercises/Modulo_2_Desenvolvimento_Front_End/bloco_15_Testes_automatizados_com_React_Testing_Library/testes-mock/src/setupTests.js
@@ -4,5 +4,6 @@
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
 // setupTests.js
-import MutationObserver from '@sheerun/mutationobserver-shim';
-window.MutationObserver = MutationObserver;
+// setupTests.js
+// import MutationObserver from '@sheerun/mutationobserver-shim';
+// window.MutationObserver = MutationObserver;
