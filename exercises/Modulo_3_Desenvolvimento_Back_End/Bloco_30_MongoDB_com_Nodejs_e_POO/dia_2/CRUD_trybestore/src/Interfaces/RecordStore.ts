9f81186ecbc0424550b6fb865a8135d6809d6ec3
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-09 11:27:48 -0300

    create RecordStore interface and connection

---
+++ exercises/Modulo_3_Desenvolvimento_Back_End/Bloco_30_MongoDB_com_Nodejs_e_POO/dia_2/CRUD_trybestore/src/Interfaces/RecordStore.ts
@@ -0,0 +1,25 @@
+import { z } from "zod";
+
+export const RecordStoreSchema = z.object({
+  artist: z.string({
+    required_error: "Artist is required",
+    invalid_type_error: "Artist must be a string",
+  }).min(3, { message: "Artist must be at least 3 characters" }),
+  title: z.string({
+    required_error: "Title is required",
+    invalid_type_error: "Title must be a string",
+  }).min(5, { message: "Title must be at least 5 characters" }),
+  format: z.string({
+    required_error: "Format is required",
+    invalid_type_error: "Format must be a string",
+  }),
+  yearPublished: z.number({
+    required_error: "'yearPublished' is required",
+    invalid_type_error: "'yearPublished' must be a number",
+  }),
+  new: z.boolean(),
+});
+
+type RecordStore = z.infer<typeof RecordStoreSchema>;
+
+export default RecordStore;
---
+++ exercises/Modulo_3_Desenvolvimento_Back_End/Bloco_30_MongoDB_com_Nodejs_e_POO/dia_2/CRUD_trybestore/src/Models/Connection.ts
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+
+const connectToDatabase = (
+  mongoDataBaseURI = process.env.MONGO_URI
+  || "mongodb://root:example@localhost:27017/TrybeStore?authSource=admin",
+  ) => mongoose.connect(mongoDataBaseURI);
