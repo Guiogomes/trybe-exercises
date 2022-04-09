47facfcb4214743cd192d7e22948be15387f6bf9
Guilherme Gomes <ggs.arquiteto@gmail.com>
2022-04-09 15:53:14 -0300

    finish implements RecordStore model and create service index file for generic Service

--- exercises/Modulo_3_Desenvolvimento_Back_End/Bloco_30_MongoDB_com_Nodejs_e_POO/dia_2/CRUD_trybestore/src/Models/RecordStore.ts
+++ exercises/Modulo_3_Desenvolvimento_Back_End/Bloco_30_MongoDB_com_Nodejs_e_POO/dia_2/CRUD_trybestore/src/Models/RecordStore.ts
@@ -1,3 +1,20 @@
-import 
-import { RecordStoreDocument, RecordStoreSchema } from "../Interfaces/RecordStore";
+import { Schema, model as createModel } from 'mongoose';
+import { RecordStoreDocument } from "../Interfaces/RecordStore";
+import RecordStore from '../Interfaces/RecordStore';
+import MongoModel from './MongoModel';
 
+const RecordStoreSchema = new Schema<RecordStoreDocument>({
+  artist: String,
+  title: String,
+  format: String,
+  yearPublished: Number,
+  new: Boolean,
+});
+
+class RecordStoreModel extends MongoModel<RecordStore> {
+  constructor(model = createModel('RecordStore', RecordStoreSchema)) {
+    super(model);
+  }
+};
+
+export default RecordStoreModel;
